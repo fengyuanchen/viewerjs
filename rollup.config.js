78d02e4aabfd149163832e83c746ac32170cdbad
Chen Fengyuan <chenfengyuan@outlook.com>
2017-01-23 19:59:50 +0800

    Use Rollup and PostCSS for building

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+const babel = require('rollup-plugin-babel');
+
+module.exports = {
+  entry: 'src/js/viewer.js',
+  targets: [
+    {
+      dest: 'dist/viewer.js',
+    },
+    {
+      dest: 'docs/js/viewer.js',
+    },
+  ],
+  format: 'umd',
+  moduleName: 'Viewer',
+  plugins: [
+    babel({
+      exclude: '/node_modules/**',
+    }),
+  ],
+  sourceMap: true,
+};
