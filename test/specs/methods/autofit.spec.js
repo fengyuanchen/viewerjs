189d33a273ed2aca46286490d372e68346383759
FishInShallow <g214946132@gmail.com>
2019-04-10 11:36:48 +0800

    fix: only touch the image to move;control image cannot leave canvas

---
+++ test/specs/methods/autofit.spec.js
@@ -0,0 +1,8 @@
+describe('autofit(method)', () => {
+  it('image should move back when it leave canvas and mouseup', (done) => {
+    done();
+  });
+  it('should not work when it is not movable', (done) => {
+    done();
+  });
+});
