0c4535f77fdb60166ac94538eb5a492c3de34149
Chen Fengyuan <chenfengyuan@outlook.com>
2026-08-22 20:18:38 +0800

    build: update ESLint configuration to enforce import sorting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,9 @@ module.exports = {
     'import/no-extraneous-dependencies': 'off',
     'no-param-reassign': 'off',
     'no-restricted-properties': 'off',
+    'sort-imports': ['error', {
+      ignoreDeclarationSort: true,
+    }],
     'valid-jsdoc': ['error', {
       requireReturn: false,
     }],
